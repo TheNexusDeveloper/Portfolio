1386721ee4368c1c0cdd41263593dd174eae167a
Kosy <okekekosy12@gmail.com>
2023-11-11 06:55:01 +0100

    update on responsiveness

--- index.js
+++ index.js
@@ -15,3 +15,12 @@ function themeChanger(){
     document.getElementById("light").style.display = 'none'
     document.getElementById("dark").style.display = 'block'
 }
+
+// function menuBar() {
+//     var x = document.getElementById("myTopnav");
+//     if (x.className === "topnav") {
+//       x.className += " responsive";
+//     } else {
+//       x.className = "topnav";
+//     }
+//   }
